5b79c70e051b4746e2ebc90af90e500d88640127
EverBrightKim <baroq8@gmail.com>
2024-09-13 23:15:24 +0900

    업카운터, 다운카운터 완성

--- src/comp/MyCounter2.js
+++ src/comp/MyCounter2.js
@@ -22,19 +22,21 @@ function useInterval(callback, delay) {
 function MyCounter2(props) {
   const [delay, setDelay] = useState(null);
 	const [count, setCount] = useState(0);
-  const callbackFn = useRef(increase);
-  function increase(){
-    setCount( count+1 )
-  }
-  function decrease(){
-    setCount( count-1 )
-  }
+  //state에 함수를 저장하려면 함수를 반환하는 함수를 제공해야한다.
+	const [cbfn, setCbfn] = useState(()=>()=>{
+    setCount((count) => count + 1);
+  });
+
+  // let increase = ()=>()=>{
+  //   setCount((count) => count + 1);
+  // };
+  // let descrease = ()=>()=>{
+  //   setCount((count) => count - 1);
+  // };
   //증가하는 카운터
   //감소하는 카운터
 
-  useInterval(() => {
-    setCount((count) => count + 1);
-  }, delay);
+  useInterval(cbfn, delay);
 
   function startCounter(e){
     setDelay(1000)
@@ -42,10 +44,22 @@ function MyCounter2(props) {
   function stopCounter(e){
     setDelay(null)
   }
+  function downCounter(e){
+    setCbfn(()=>()=>{
+      setCount((count) => count - 1);
+    })
+  }
+  function upCounter(e){
+    setCbfn(()=>()=>{
+      setCount((count) => count + 1);
+    })
+  }
   return (
       <div>
           <button onClick={startCounter}>시작</button>
           <button onClick={stopCounter}>중지</button>
+          <button onClick={upCounter}>올라가는 카운터</button>
+          <button onClick={downCounter}>내려가는 카운터</button>
           <h1>{count}</h1>
       </div>
   );
